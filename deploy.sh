30e253e815fbbed8549edd2c57f3b9e988a74515
Tommaso Leonardi - farm <tl344@ebi.ac.uk>
2014-07-04 13:55:21 +0100

    Initial commit

---
+++ deploy.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+if [[ -z $HOME ]]; then
+	echo "HOME variable is not set. Quitting.";
+	exit 1;
+fi;
+
+NOW=$(date +"%m_%d_%Y-%k_%M_%S")
+
+mkdir -p $HOME/.old_conf
+
+if [[ -f $HOME/.dotfiles/bash/bashrc ]]; then
+	if [[ -f $HOME/.bashrc ]]; then
+		cp -L $HOME/.bashrc $HOME/.old_conf/bashrc_$NOW && rm $HOME/.bashrc;
+	fi;
+	echo "Linking $HOME/.bashrc to $HOME/.dotfiles/bash/bashrc"
+	ln -s $HOME/.dotfiles/bash/bashrc $HOME/.bashrc
+else
+	echo "$HOME/.dotfiles/bash/bashrc: no such file";
+fi
+
+
+if [[ -f $HOME/.dotfiles/tmux/tmux.conf ]]; then
+	if [[ -f $HOME/.tmux.conf ]]; then
+		cp -L $HOME/.tmux.conf $HOME/.old_conf/tmux.conf_$NOW && rm $HOME/.tmux.conf;
+	fi;
+	echo "Linking $HOME/.tmux.conf to $HOME/.dotfiles/tmux/tmux.conf"
+	ln -s $HOME/.dotfiles/tmux/tmux.conf $HOME/.tmux.conf
+else
+	echo "$HOME/.dotfiles/tmux/tmux.conf: no such file";
+fi
+
+
+if [[ -f $HOME/.dotfiles/vim/vimrc ]]; then
+	if [[ -f $HOME/.vimrc ]]; then
+		cp -L $HOME/.vimrc $HOME/.old_conf/vimrc_$NOW && rm $HOME/.vimrc;
+	fi;
+	echo "Linking $HOME/.vimrc to $HOME/.dotfiles/vim/vimrc"
+	ln -s $HOME/.dotfiles/vim/vimrc $HOME/.vimrc
+else
+	echo "$HOME/.dotfiles/vim/vimrc: no such file";
+fi
